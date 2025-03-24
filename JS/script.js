9109837e0e7a361bf1781cac13bdf53343c004f8
rodrigowuoll <rocapelini.rdc@gmail.com>
2025-03-24 17:26:37 -0300

    Atualização

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    // Alterna a classe dark-theme no body
+    document.body.classList.toggle('dark-theme');
+});
